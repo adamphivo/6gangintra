e7e5c01625180cc1e20d2e0ce5db9657ffebf9bf
Adam Phi Vo <adamphivo@gmail.com>
2020-10-24 12:02:01 +0200

    Add Password Encription in Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,18 +3,9 @@
 // Fixtures - Populate the database with fake data using faker package.
 // php bin/console doctrine:fixtures:load -> Update database with fake data.
 
-// USING FAKE
-// $faker = Faker\Factory::create();
-// generate data by accessing properties
-// echo $faker->name;
-// 'Lucy Cechtelar';
-// echo $faker->address;
-// "426 Jordy Lodge
-// Cartwrightshire, SC 88120-6700"
-// echo $faker->text;
-
 namespace App\DataFixtures;
 
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Post;
@@ -25,29 +16,35 @@ use Indenter;
 
 class AppFixtures extends Fixture
 {
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $posts[$i] = new Product();
-        // $manager->persist($posts[$i]);
         // Creating the factory
         $faker = Faker\Factory::create('fr_FR');
         $indenter = new \Gajus\Dindent\Indenter();
 
+        // Populate users
         $users = Array();
-        // Users
         for($i = 0; $i < 50; $i++){
             $users[$i] = new User();
             $users[$i]->setExperience($faker->numberBetween($min = 0, $max = 1000));
             $users[$i]->setEmail($faker->email);
             $users[$i]->setPortraitUrl("https://picsum.photos/120/120/?blur");
             $users[$i]->setUserName($faker->userName);
-            $users[$i]->setPassword($faker->password);
+            $users[$i]->setPassword($this->encoder->encodePassword($users[$i],$faker->password));
 
             $manager->persist($users[$i]);
         }
 
+        // Populate posts
         $posts = Array();
-        // Posts
         for($i = 0; $i < 500; $i++){
             $posts[$i] = new Post();
             $posts[$i]->setTitle($faker->catchPhrase);
@@ -58,7 +55,6 @@ class AppFixtures extends Fixture
 
             $manager->persist($posts[$i]);
         }
-
 
         // Make changes
         $manager->flush();
